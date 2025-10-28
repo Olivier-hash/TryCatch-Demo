7437890a09fd8f3320d8ce68aea852e4db2cd6ca
olivier Rukundo <rr.olivier01@gmail.com>
2025-10-28 22:18:29 +0100

    Projektdateien hinzufügen.

---
+++ Try-catch-block/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProjectName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte trage eine Zahl ein");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (Exception) // alt: FormatException, ArgumentNullException, OverflowException
+            {
+                Console.WriteLine("Fehrer aufgetreten! z.b eingegeben wort wer war falsch");
+            }
+
+            Console.ReadLine();
+            
+        }
+       
+    }
+}
