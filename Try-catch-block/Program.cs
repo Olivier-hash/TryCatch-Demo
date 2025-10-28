64bbd6c8d40babb5f66b848b7f8f5cccef6a4020
olivier Rukundo <rr.olivier01@gmail.com>
2025-10-28 22:37:05 +0100

    Added multiple exception handling for user input with proper catch order

--- Try-catch-block/Program.cs
+++ Try-catch-block/Program.cs
@@ -13,10 +13,25 @@ namespace ProjectName
             {
                 int userInputAsInt = int.Parse(userInput);
             }
-            catch (Exception) // alt: FormatException, ArgumentNullException, OverflowException
+            // alt: FormatException, ArgumentNullException, OverflowException, Exception ex
+
+            catch (FormatException)
+            {
+                Console.WriteLine("format war inkorrekt: Du hattest eine Nummer eintragen sollen ");
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("ArgumentException: der welt war leer(null");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("OverflowException, der eingegeben wert, war zu lang");
+            }
+            catch (Exception) 
             {
                 Console.WriteLine("Fehrer aufgetreten! z.b eingegeben wort wer war falsch");
             }
+
 
             Console.ReadLine();
             
